aaf98e62d55c8b0af8703c4a7d06c17b3a18fb10
manolobkno08 <manologm10@hotmail.com>
2021-10-26 13:07:39 -0500

    task0 fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t temp, i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -20,6 +23,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
