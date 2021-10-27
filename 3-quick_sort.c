9c5e8e9a23051a3fd8e6ed0a6947ad7d6326cd39
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-26 21:38:49 -0500

    update file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
-void quick_sort_recursive(int *array, int left_limit, int right_limit);
-void swap(int *array, int a, int b);
+void quick_sort_recursive(int *array, int left_limit, int right_limit, size_t size);
+void swap(int *array, int a, int b, size_t size);
 /**
  * quick_sort - organize an array using the quick sort method
  * @array: array to organize
@@ -8,7 +8,7 @@ void swap(int *array, int a, int b);
  */
 void quick_sort(int *array, size_t size)
 {
-    quick_sort_recursive(array, 0, size - 1);
+    quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
@@ -17,7 +17,7 @@ void quick_sort(int *array, size_t size)
  * @left_limit: saves the position on the left
  * @right_limit: saves the position on the right
  */
-void quick_sort_recursive(int *array, int left_limit, int right_limit)
+void quick_sort_recursive(int *array, int left_limit, int right_limit, size_t size)
 {
     int pivot, left, right;
 
@@ -39,25 +39,22 @@ void quick_sort_recursive(int *array, int left_limit, int right_limit)
         
         if (left <= right)
         {
-            swap(array, left, right);
-
+            swap(array, left, right, size);
             left++;
             right--;
         }
-        
         
     } while (left <= right);
     
     if (left_limit < right_limit)
     {
-        quick_sort_recursive(array, left_limit, right);
+        quick_sort_recursive(array, left_limit, right, size);
     }
     
     if (right_limit > left)
     {
-        quick_sort_recursive(array, left, right_limit);
-    }   
-
+        quick_sort_recursive(array, left, right_limit, size);
+    }
 }
 /**
  * swap - swaps values in an array
@@ -65,9 +62,10 @@ void quick_sort_recursive(int *array, int left_limit, int right_limit)
  * @a: saves the position to be exchanged
  * @b: saves the position to be exchanged
  */
-void swap(int *array, int a, int b)
+void swap(int *array, int a, int b, size_t size)
 {
     int temp = array[a];
     array[a] = array[b];
     array[b] = temp;
+	print_array(array, size);
 }
